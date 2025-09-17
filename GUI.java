22479a788fa4348ce110aacdbbd18e7d307121b1
Kekeli <agblobik@gmail.com>
2025-09-17 21:08:27 +0000

    commit

---
+++ GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+public class GUI{
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Lets Gooo");
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null); // absolute positioning
+
+        frame.setVisible(true);
+
+
+        
+    }
+}
---
+++ jcode.java
@@ -0,0 +1,9 @@
+public class jcode{
+    public static void main(String[] args) {
+        int number=1;
+        System.out.println(number++ +4);
+        System.out.println(number +4);
+        System.out.println(++number +4);
+    }
+
+}
